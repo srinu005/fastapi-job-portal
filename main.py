3303296b2b2b879c94a2e5cb1e55c9cca99dad07
srinu <srinuramatenki20@gmail.com>
2026-02-28 17:47:43 +0530

    Implemented PostgreSQL persistence with Docker Compose

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
 from typing import List
+
+# Import our custom database tools and models
+import models
+from database import engine, get_db
 from schemas import JobCreate, JobResponse
 
-app = FastAPI( title ='Job Board API')
+# 1. This line creates the actual tables in Postgres
+# It looks at models.py and builds the table if it doesn't exist
+models.Base.metadata.create_all(bind=engine)
 
-#Temporary Database
+app = FastAPI(title="Job Board with Postgres")
 
-jobs_db = []
-id_counter = 1
 @app.get("/")
-async def home():
-    return {"message": "JOB Board is Online"}
+def home():
+    return {"message": "Job Board is Connected to Postgres!"}
 
-@app.post("/jobs/", response_model = JobResponse)
-async def create_job( job: JobCreate):
-    global id_counter
-    new_job = job.model_dump()
-    new_job["id"] = id_counter
-    jobs_db.append(new_job)
-    id_counter += 1
-    return new_job
+# CREATE: Add a job to the database
+@app.post("/jobs/", response_model=JobResponse)
+def create_job(job: JobCreate, db: Session = Depends(get_db)):
+    # Create a "Database Object" from our user input
+    db_job = models.JobModel(
+        title=job.title,
+        company=job.company,
+        location=job.location,
+        salary=job.salary,
+        is_remote=job.is_remote
+    )
+    db.add(db_job)        # Tell Postgres: "I want to add this"
+    db.commit()          # Tell Postgres: "Save it permanently now"
+    db.refresh(db_job)   # Get the new ID that Postgres generated
+    return db_job
 
-@app.get("/jobs/", response_model = List[JobResponse])
-async def get_jobs():
-    return jobs_db
-
+# READ: Get all jobs from the database
+@app.get("/jobs/", response_model=List[JobResponse])
+def get_jobs(db: Session = Depends(get_db)):
+    # This is like writing "SELECT * FROM jobs" in SQL
+    jobs = db.query(models.JobModel).all()
+    return jobs
